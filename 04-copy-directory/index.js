026f8a92dcdf3d71d4345805d40f869d9494fd50
weronika-fed <nikafed3@gmail.com>
2021-11-10 02:38:07 +0300

    fix: fix copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,21 +3,24 @@ const path = require('path');
 const source = path.join(__dirname, 'files-copy');
 const files = './04-copy-directory/files/';
 
-fs.mkdir(source, { recursive: true }, err => {
+fs.rm(source, { recursive: true, force: true }, err => {
+    fs.mkdir(source, { recursive: true }, err => {
 
-    console.log(`Папка ${source} успешно создана`);
-    fs.readdir(files, (err, files) => {
-        files.forEach(file => {
-            let oldFile = path.join(__dirname, 'files', file);
-            let newFile = path.join(__dirname, 'files-copy', file);
-            fs.copyFile(oldFile, newFile, err => {
-                if (err) throw err;
-                console.log(`Файл ${file} успешно скопирован`);
+        console.log(`Папка ${source} успешно создана`);
+        fs.readdir(files, (err, files) => {
+            files.forEach(file => {
+                let oldFile = path.join(__dirname, 'files', file);
+                let newFile = path.join(__dirname, 'files-copy', file);
+                fs.copyFile(oldFile, newFile, err => {
+                    if (err) throw err;
+                    console.log(`Файл ${file} успешно скопирован`);
+                });
             });
-        });
 
+        });
     });
 });
+
 
 
 
